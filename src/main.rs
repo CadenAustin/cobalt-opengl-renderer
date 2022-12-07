f318d3ceb2cb4288481caa82ce6513859c1a98b7
CadenAustin <cadenbryant@hotmail.com>
2022-12-06 18:07:58 -0600

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
-use miniquad::*;
-use stage::Stage;
-
 const WIDTH: i32 = 1920;
 const HEIGHT: i32 = 1080;
 
-mod stage;
-mod shader;
-mod scene;
-mod camera;
-mod egui_menu;
-mod material;
-
 fn main() {
-    let conf = conf::Conf {
-        window_title: "Cobalt".into(),
-        window_height: HEIGHT,
-        window_width: WIDTH,
-        ..Default::default()
-    };
-    miniquad::start(conf, |mut ctx| {
-        Box::new(Stage::new(&mut ctx))
-    });
+    
 }
